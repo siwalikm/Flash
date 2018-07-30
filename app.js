ad4b4f3deaebf71a7f30cd0fe815286faf1cdd4c
siwalikm <siwalikm@gmail.com>
2018-07-31 00:06:27 +0530

    imp. fixes
    - replace gif with mp4
    - setup lint
    - refactor & bugfix
    - google page analytics

--- app.js
+++ app.js
@@ -1,71 +1,73 @@
-const express = require('express');
+var express = require('express');
+
 var app = express();
-var path = require("path");
+var path = require('path');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers',
-    'Origin,X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers',
+        'Origin,X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 });
+
+
+function randomDelayInMS() {
+    // random delay in milliseconds between 0 to 10000 ms
+    return (Math.floor(Math.random() * 10) + 0) * 1000;
+}
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-  // res.redirect('https://www.google.com');
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 app.get('/doc', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/doc.html'));
-  // res.redirect('https://www.google.com');
+    res.sendFile(path.join(__dirname, '/public/doc.html'));
 });
 
 
 app.get('/delay/:delayValue/', (req, res) => {
 
-  if (req.params.delayValue === 'random') {
-    req.params.delayValue = randomDelayInMS();
-  } else if (!isNaN(req.params.delayValue)) {
-    setTimeout(() => {
-      res.send({
-        status: 200,
-        delay: `${req.params.delayValue} ms`,
-        message: `Mock response from Flash`
-      });
-    }, req.params.delayValue);
-  } else {
-    res.status(500).send(`Delay value should be valid number(in milliseconds) or 'random'`);
-  }
+    if (req.params.delayValue === 'random') {
+        req.params.delayValue = randomDelayInMS();
+    } else if (!isNaN(req.params.delayValue)) {
+        setTimeout(() => {
+            res.send({
+                status: 200,
+                delay: `${req.params.delayValue} ms`,
+                message: 'Mock response from Flash'
+            });
+        }, req.params.delayValue);
+    } else {
+        res.status(500).send('Delay value should be valid number(in milliseconds) or "random"');
+    }
 
-})
+});
 
 app.get('/delay/:delayValue/url/:urlValue*', (req, res) => {
+    var url = `${req.params.urlValue}${req.params[0]}`;
 
-  if (req.params.delayValue === 'random') {
-    req.params.delayValue = randomDelayInMS();
-  } else if (!isNaN(req.params.delayValue)) {
-    res.status(302);
-    setTimeout(() => {
-      if (req.params[0].length) {
-        res.redirect(`${req.params.urlValue}${req.params[0]}`);
-      } else {
-        res.redirect(`http://${req.params.urlValue}`);
-      }
-    }, req.params.delayValue);
-  } else {
-    res.status(500).send(`Delay value should be valid number(in milliseconds) or 'random'`);
-  }
+    if (req.params.delayValue === 'random') {
+        req.params.delayValue = randomDelayInMS();
+    } else if (!isNaN(req.params.delayValue)) {
+        setTimeout(() => {
+
+            if (!url.match('(http|https)://') && !url.match('://')) {
+                url = `http://${url}`;
+            }
+            res.redirect(url);
+        }, req.params.delayValue);
+    } else {
+        res.status(500).send('Delay value should be valid number(in milliseconds) or "random"');
+    }
 
 });
 
 app.get('*', function (req, res) {
-  res.redirect('/');
+    res.redirect('/');
 });
 app.set('port', process.env.PORT || 3000);
+// eslint-disable-next-line
 app.listen(app.get('port'), () => console.log('Flash listening on port ' + app.get('port')));
-
-function randomDelayInMS() {
-  // random delay in milliseconds between 0 to 10000 ms
-  return (Math.floor(Math.random() * 10) + 0) * 1000;
-}
--- public/js/scribbler.js
+++ public/js/scribbler.js
@@ -3,24 +3,33 @@ window.hostUrl = window.location.origin;
 
 // utilities
 var get = function (selector, scope) {
-  scope = scope ? scope : document;
-  return scope.querySelector(selector);
+    scope = scope ? scope : document;
+    return scope.querySelector(selector);
 };
 
 var getAll = function (selector, scope) {
-  scope = scope ? scope : document;
-  return scope.querySelectorAll(selector);
+    scope = scope ? scope : document;
+    return scope.querySelectorAll(selector);
+};
+
+var typeItOut = function () {
+    if (i < txt.length) {
+        document.getElementsByClassName('demo')[0].innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeItOut, speed);
+
+    }
 };
 
 jQuery(document).ready(() => {
-  $('.hostUrl').text(window.hostUrl);
-  $.get('https://api.github.com/repos/siwalikm/FLASH/releases',
-    data => {
-      if (data.length) {
-        data.forEach(releaseItem => {
-          var newDate = new Date(releaseItem.published_at).toDateString();
-          var body = releaseItem.body.replace(/\n/g, "<br />");
-          var domItem = `<div class="changelog__item">
+    jQuery('.hostUrl').text(window.hostUrl);
+    jQuery.get('https://api.github.com/repos/siwalikm/FLASH/releases',
+        data => {
+            if (data.length) {
+                data.forEach(releaseItem => {
+                    var newDate = new Date(releaseItem.published_at).toDateString();
+                    var body = releaseItem.body.replace(/\n/g, '<br />');
+                    var domItem = `<div class="changelog__item">
           <div class="changelog__meta">
             <h4 class="changelog__title">${releaseItem.name}</h4>
             <small class="changelog__date">${newDate}</small>
@@ -31,20 +40,22 @@ jQuery(document).ready(() => {
             </span>
           </div>
         </div> `;
-          jQuery('.changelog .items').append(domItem);
+                    jQuery('.changelog .items').append(domItem);
+                });
+            } else {
+                jQuery('.changelog').hide();
+            }
         });
-      } else {
-        jQuery('.changelog').hide();
-      }
-    });
 
 
 });
 
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
-  var i = 0;
-  var txt = `// GET() mock response after specific delay
+    var i = 0;
+    var speed = 60;
+    setTimeout(typeItOut, 1800);
+    var txt = `// GET() mock response after specific delay
   > $.get(${window.hostUrl}/delay/3000', data=> {console.log(data)});
 
   // api response
@@ -60,97 +71,57 @@ if (document.getElementsByClassName('demo').length > 0) {
   > $.get('${window.hostUrl}/delay/1000/url/api.github.com/users/siwalikm', data=> {console.log(data)});
 
   // api response
-  > {login: "siwalikm", id: 20061595, node_id: "MDQ6VXNlcjIwMDYxNTk1", …}
-
-
-  
-                `;
-  var block = `// after 3000ms delay
-  ▶ { login: "siwalikm", id: 20061595, ... }`;
-  var speed = 60;
-
-  function typeItOut() {
-    if (i < txt.length) {
-      document.getElementsByClassName('demo')[0].innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeItOut, speed);
-
-    }
-  }
-
-  setTimeout(typeItOut, 1800);
+  > {login: "siwalikm", id: 20061595, node_id: "MDQ6VXNlcjIwMDYxNTk1",…}
+`;
 
 }
 
 // toggle tabs on codeblock
-window.addEventListener("load", function () {
-  // get all tab_containers in the document
-  var tabContainers = getAll(".tab__container");
+window.addEventListener('load', function () {
+    // get all tab_containers in the document
+    var tabContainers = getAll('.tab__container');
 
-  // bind click event to each tab container
-  for (var i = 0; i < tabContainers.length; i++) {
-    get('.tab__menu', tabContainers[i]).addEventListener("click", tabClick);
-  }
-
-  // each click event is scoped to the tab_container
-  function tabClick(event) {
-    var scope = event.currentTarget.parentNode;
-    var clickedTab = event.target;
-    var tabs = getAll('.tab', scope);
-    var panes = getAll('.tab__pane', scope);
-    var activePane = get(`.${clickedTab.getAttribute('data-tab')}`, scope);
-
-    // remove all active tab classes
-    for (var i = 0; i < tabs.length; i++) {
-      tabs[i].classList.remove('active');
+    // bind click event to each tab container
+    for (var i = 0; i < tabContainers.length; i++) {
+        get('.tab__menu', tabContainers[i]).addEventListener('click', tabClick);
     }
 
-    // remove all active pane classes
-    for (var i = 0; i < panes.length; i++) {
-      panes[i].classList.remove('active');
-    }
+    // each click event is scoped to the tab_container
+    function tabClick(event) {
+        var scope = event.currentTarget.parentNode;
+        var clickedTab = event.target;
+        var tabs = getAll('.tab', scope);
+        var panes = getAll('.tab__pane', scope);
+        var activePane = get(`.${clickedTab.getAttribute('data-tab')}`, scope);
 
-    // apply active classes on desired tab and pane
-    clickedTab.classList.add('active');
-    activePane.classList.add('active');
-  }
+        // remove all active tab classes
+        for (var i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove('active');
+        }
+
+        // remove all active pane classes
+        for (var j = 0; j < panes.length; j++) {
+            panes[j].classList.remove('active');
+        }
+
+        // apply active classes on desired tab and pane
+        clickedTab.classList.add('active');
+        activePane.classList.add('active');
+    }
 });
-
-//in page scrolling for documentaiton page
-var btns = getAll('.js-btn');
-var sections = getAll('.js-section');
-
-function setActiveLink(event) {
-  // remove all active tab classes
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].classList.remove('selected');
-  }
-
-  event.target.classList.add('selected');
-}
-
-function smoothScrollTo(element, event) {
-  setActiveLink(event);
-
-  window.scrollTo({
-    'behavior': 'smooth',
-    'top': element.offsetTop - 20,
-    'left': 0
-  });
-}
 
 
 // fix menu to page-top once user starts scrolling
 window.addEventListener('scroll', function () {
-  var docNav = get('.doc__nav > ul');
+    var docNav = get('.doc__nav > ul');
 
-  if (docNav) {
-    if (window.pageYOffset > 63) {
-      docNav.classList.add('fixed');
-    } else {
-      docNav.classList.remove('fixed');
+    if (docNav) {
+        if (window.pageYOffset > 63) {
+            docNav.classList.add('fixed');
+        } else {
+            docNav.classList.remove('fixed');
+        }
     }
-  }
 });
 
 // responsive navigation
@@ -158,14 +129,14 @@ var topNav = get('.menu');
 var icon = get('.toggle');
 
 window.addEventListener('load', function () {
-  function showNav() {
-    if (topNav.className === 'menu') {
-      topNav.className += ' responsive';
-      icon.className += ' open';
-    } else {
-      topNav.className = 'menu';
-      icon.classList.remove('open');
+    function showNav() {
+        if (topNav.className === 'menu') {
+            topNav.className += ' responsive';
+            icon.className += ' open';
+        } else {
+            topNav.className = 'menu';
+            icon.classList.remove('open');
+        }
     }
-  }
-  icon.addEventListener('click', showNav);
+    icon.addEventListener('click', showNav);
 });
